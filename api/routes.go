008e4f141ee11eaa909db687556b54bffb94e4d3
sumi126 <imratuzzahansumi@gmail.com>
2025-07-03 09:38:35 +0600

    cors setup

--- api/routes.go
+++ api/routes.go
@@ -11,7 +11,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		AllowCredentials: false,
